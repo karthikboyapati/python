6ffb1c181d189b8d98800e1bbcca0d8bb26162be
KarthikB <29143529+karthikboyapati@users.noreply.github.com>
2019-04-01 02:37:44 +0530

    Create age.py
    created the script file age.py

---
+++ age.py
@@ -0,0 +1,6 @@
+a = int(input('Please enter your age:'))
+b = str((100 - a) + 2019)
+if(a<100):{
+print("Haha!! What a sucker!! You will turn 100 by " + str(b))}
+if(a>150):{
+print("You are already dead! Are you a ghost or what!!!")}
