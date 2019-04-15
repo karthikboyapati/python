2a5333c99c7b3bd996a4c0acd89f8aa1687cdfba
KarthikB <29143529+karthikboyapati@users.noreply.github.com>
2019-04-16 01:44:13 +0530

    Update Weightedmean.py

--- Weightedmean.py
+++ Weightedmean.py
@@ -13,3 +13,15 @@ den = sum(w)
 
 #Rounded off to value 1
 print(round((num/den),1))
+
+
+#Code with numpy library
+import numpy as np
+
+num = input()
+x = list(map(int,input().split()))
+w = list(map(int,input().split()))
+#method for weighted mean "np.average"
+xw = np.average(x,weights=w)
+#rounded off the value to 1
+print(round(xw,1))
